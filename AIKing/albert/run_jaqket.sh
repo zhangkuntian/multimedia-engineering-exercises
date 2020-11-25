f0804c5a38359d5286238d65d9e0d5aefacfb5cf
m-yoshinaka <49668018+m-yoshinaka@users.noreply.github.com>
2020-11-25 05:01:06 +0000

    [add] Add scripts for Albert

---
+++ AIKing/albert/run_jaqket.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES="4"
+DDIR=../data/
+OUTDIR=output_dir_01
+
+TRAIN=train_questions.json
+DEV=dev1_questions.json
+TEST=dev2_questions.json
+ENTITY=candidate_entities.json.gz
+
+MODEL="ALINEAR/albert-japanese-v2"
+MODEL_TYPE="albert"
+# MODEL="bandainamco-mirai/distilbert-base-japanese"
+# MODEL_TYPE="distilbert"
+# MODEL="cl-tohoku/bert-base-japanese-whole-word-masking"
+# MODEL_TYPE="bert"
+
+python3 jaqket_albert_distilbert.py  \
+  --data_dir ${DDIR} \
+  --model_name_or_path ${MODEL} \
+  --model_type ${MODEL_TYPE} \
+  --task_name jaqket \
+  --entities_fname ${ENTITY} \
+  --train_fname ${TRAIN} \
+  --dev_fname ${DEV} \
+  --test_fname ${TEST} \
+  --output_dir ${OUTDIR} \
+  --train_num_options 20 \
+  --do_train \
+  --do_eval \
+  --do_test \
+  --learning_rate=3e-5 \
+  --per_gpu_train_batch_size 1 \
+  --gradient_accumulation_steps 8 \
+  --num_train_epochs 10 \
+  --overwrite_output_dir \
+  --logging_steps 10 \
+  --save_steps 5000
